58697c6adaca7c8d2d65bb2122948a00d8c6a253
Joursoir <triste.deal@mail.ru>
2020-05-28 16:26:48 +0300

    alpha launcher

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+using System.Security;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.Diagnostics;
+
+namespace SuperDesign
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        IniFiles manager = new IniFiles(AppDomain.CurrentDomain.BaseDirectory + "rev.ini");
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            // сначала грузится, а потом уже элементы к нему (не факт)
+            // MessageBox.Show("Text");
+
+            string nickname = manager.ReadINI("steamclient", "PlayerName");
+            loginBox.Text = nickname;
+        }
+
+        private void PlayButton(object sender, EventArgs e)
+        {
+            if (loginBox.Text.Length < 3) // меняем на стандартное имя
+                loginBox.Text = "cssLauncherPlayer";
+
+            manager.Write("steamclient", "PlayerName", loginBox.Text);
+            CMD("start Run_CSS.exe");
+                // CMD("start " + Settings.GameFolder + "-steam -game cstrike");
+
+                //ProcessStartInfo startInfo = new ProcessStartInfo(Settings.GameFolder, "-steam -game cstrike -russian");
+
+                //Process.Start(startInfo).WaitForExit();
+                /*Process proc = new Process();
+                proc.StartInfo.FileName = @"C:\Games\Counter-Strike Source v91\start_css.bat";
+                proc.StartInfo.WorkingDirectory = @"C:\Games\Counter-Strike Source v91";
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();*/
+        }
+        private void CMD(string line)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = $"/c {line}",
+                WindowStyle = ProcessWindowStyle.Hidden
+            }).WaitForExit();
+        }
+
+        private void Click_Settings(object sender, EventArgs e)
+        {
+            Settings_Window new_window = new Settings_Window();
+            new_window.ShowDialog(); // делаем недоступным старое окно, при вызове "настройки"
+        }
+    }
+
+    public class IniFiles
+    {
+
+        string Path; //Имя файла.
+
+        [DllImport("kernel32", CharSet = CharSet.Auto)] // Подключаем kernel32.dll и описываем его функцию WritePrivateProfilesString
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Auto)] // Еще раз подключаем kernel32.dll, а теперь описываем функцию GetPrivateProfileString
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        static extern uint GetPrivateProfileSection(string lpAppName, IntPtr lpReturnedString, uint nSize, string lpFileName);
+        // С помощью конструктора записываем пусть до файла и его имя.
+        public IniFiles(string IniPath)
+        {
+            Path = new FileInfo(IniPath).FullName.ToString();
+        }
+
+        //Читаем ini-файл и возвращаем значение указного ключа из заданной секции.
+        public string ReadINI(string Section, string Key)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
+        //Записываем в ini-файл. Запись происходит в выбранную секцию в выбранный ключ.
+        public void Write(string Section, string Key, string Value)
+        {
+            WritePrivateProfileString(Section, Key, Value, Path);
+        }
+
+        //Удаляем ключ из выбранной секции.
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Section, Key, null);
+        }
+        //Удаляем выбранную секцию
+        public void DeleteSection(string Section = null)
+        {
+            Write(Section, null, null);
+        }
+        //Проверяем, есть ли такой ключ, в этой секции
+        public bool KeyExists(string Section, string Key)
+        {
+            return ReadINI(Section, Key).Length > 0;
+        }
+
+        //читаем в массив все пары ключей-значнией в данной секции
+        public bool GetPrivateProfileSection(string appName, string fileName, out string[] section)
+        {
+            section = null;
+
+            if (!System.IO.File.Exists(fileName))
+                return false;
+
+            const uint MAX_BUFFER = 32767;
+
+            IntPtr pReturnedString = Marshal.AllocCoTaskMem((int)MAX_BUFFER * sizeof(char));
+
+            uint bytesReturned = GetPrivateProfileSection(appName, pReturnedString, MAX_BUFFER, fileName);
+
+            if ((bytesReturned == MAX_BUFFER - 2) || (bytesReturned == 0))
+            {
+                Marshal.FreeCoTaskMem(pReturnedString);
+                return false;
+            }
+            string returnedString = Marshal.PtrToStringAuto(pReturnedString, (int)(bytesReturned - 1));
+
+            section = returnedString.Split('\0');
+
+            Marshal.FreeCoTaskMem(pReturnedString);
+            return true;
+        }
+    }
+}
---
+++ Settings_Window.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace SuperDesign
+{
+    /// <summary>
+    /// Логика взаимодействия для Settings_Window.xaml
+    /// </summary>
+    public partial class Settings_Window : MetroWindow
+    {
+        IniFiles manager = new IniFiles(AppDomain.CurrentDomain.BaseDirectory + "rev.ini");
+        private string new_file_folder = AppDomain.CurrentDomain.BaseDirectory + "platform\\avatar.dat";
+        private string rez_file_folder = AppDomain.CurrentDomain.BaseDirectory + "platform\\avatar1.dat";
+        private Image pAvatar;
+        public Settings_Window()
+        {
+            InitializeComponent();
+            s_language.ItemsSource = new List<string> { "Russian", "English", "French", "Italian", "German", "Spanish", "Portugeuse" };
+            /* ставим стандартное значение: */
+
+            // язык игры:
+            string language = manager.ReadINI("Emulator", "Language");
+            int index = s_language.Items.IndexOf(language);
+            s_language.SelectedIndex = index;
+
+            // строка запуска:
+            string launch_line = manager.ReadINI("Loader", "ProcName");
+            s_line.Text = launch_line;
+
+            // клан-тег:
+            string clan_tag = manager.ReadINI("steamclient", "ClanTag");
+            s_clantag.Text = clan_tag;
+
+            // показ аватара:
+            bool show_avatar = Convert.ToBoolean(manager.ReadINI("steamclient", "Use_avatar"));
+            s_avatar.IsChecked = show_avatar;
+        }
+
+        private void ButtonBase_Loaded(object sender, EventArgs e)
+        {
+            // аватар:
+            pAvatar = buttonAvatar.Template.FindName("imageAvatar", buttonAvatar) as Image;
+            if (System.IO.File.Exists(new_file_folder))
+            {
+                System.IO.File.Copy(new_file_folder, rez_file_folder, true);
+
+                BitmapImage bitmapImage = GetBitmapImage(rez_file_folder);
+                pAvatar.Source = bitmapImage;
+                System.IO.File.Delete(rez_file_folder);
+                System.IO.File.Copy(new_file_folder, rez_file_folder, true);
+            }
+
+
+        }
+
+        private void buttonAvatar_MouseMove(object sender, MouseEventArgs e) => this.Cursor = Cursors.Hand;
+        private void buttonAvatar_MouseLeave(object sender, MouseEventArgs e) => this.Cursor = Cursors.Arrow;
+
+        private void AcceptChange(object sender, EventArgs e)
+        {
+            manager.Write("Emulator", "Language", s_language.SelectedItem.ToString());
+            manager.Write("Loader", "ProcName", s_line.Text);
+            manager.Write("steamclient", "ClanTag", s_clantag.Text);
+            manager.Write("steamclient", "Use_avatar", s_avatar.IsChecked.ToString());
+            
+            this.Close();
+        }
+
+        private void ChangeAvatar(object sender, EventArgs e)
+        {
+            OpenFileDialog aFile = new OpenFileDialog(); // aFile = avatarFile
+            aFile.Filter = "Image (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+            aFile.RestoreDirectory = true;
+
+            // Show open file dialog box
+            Nullable<bool> result = aFile.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string file_folder = aFile.FileName;
+
+                // переименовываем, перемещаем, меняем формат:
+                System.IO.File.Copy(file_folder, new_file_folder, true);
+                System.IO.File.Copy(file_folder, rez_file_folder, true);
+
+                // string new_file_folder = System.IO.Path.ChangeExtension(file_folder, ".dat"); // меняет формат
+
+                BitmapImage bitmapImage = GetBitmapImage(rez_file_folder);
+                pAvatar.Source = bitmapImage;
+                System.IO.File.Delete(rez_file_folder);
+
+
+            }
+        }
+        private void ResetConsoleLine(object sender, EventArgs e)
+        {
+            s_line.Text = "hl2.exe -game cstrike -steam -silent -novid -high";
+        }
+
+        public static BitmapImage GetBitmapImage(string str)
+        {
+            var bitmap = new BitmapImage();
+            using (var stream = new System.IO.FileStream(str, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+            {
+                bitmap.BeginInit();
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.StreamSource = stream;
+                bitmap.EndInit();
+                bitmap.Freeze();
+            }
+            return bitmap;
+        }
+    }
+    public class MyControl : Control
+    {
+        static MyControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(MyControl),
+                new FrameworkPropertyMetadata(typeof(MyControl)));
+        }
+
+        public static readonly DependencyProperty DisplayProperty = DependencyProperty.Register(
+            "Display", typeof(ItemsControl), typeof(MyControl));
+
+        public ItemsControl Display
+        {
+            get
+            {
+                return (ItemsControl)this.GetValue(MyControl.DisplayProperty);
+            }
+            set
+            {
+                this.SetValue(MyControl.DisplayProperty, value);
+            }
+        }
+    }
+}
